b59700d09e15b72a92f0de65a0f255d4fe0f6714
Patrick Honkonen <rizzin@gmail.com>
2024-11-01 20:24:20 -0400

    [TROUP-30] Add kover code coverage
    Adds kover code coverage support to the project.
    
    This includes:
    
    - Adding the kover plugin and dependency to the build.
    - Updating the build configuration to generate coverage reports.
    - Updating the podspec to disable user script sandboxing.
    - Updating the CircleCI configuration to run the coverage task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.kotlinCocoapods).apply(false)
+    alias(libs.plugins.kover).apply(false)
 }
